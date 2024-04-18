cf387e797ceebaba87e23f5db700cd3f5e8a6dee
bereket-7 <bekhonelign@gmail.com>
2024-04-18 03:27:28 +0300

    import necessary imports

--- base/views.py
+++ base/views.py
@@ -1,9 +1,15 @@
-from django.shortcuts import render
+
+from django.urls import reverse_lazy
+from django.shortcuts import render, redirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
-from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 
+
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 from django.views import View
 from django.shortcuts import redirect
